a92dd4f3fa08e99c640d6720e1585f5d0af95273
C-Xiaohe <1207878996@qq.com>
2022-02-23 00:00:13 +0800

    分支与循环

--- test-2.22/test-2.22/test-2.22.c
+++ test-2.22/test-2.22/test-2.22.c
@@ -1,19 +1,41 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include<stdio.h>
+#include<string.h>
 int main()
 {
-	int c = 0;
-	int b = 1;
-	int i = 0;
-	for (i = 1; i <=3; i++)
+	char arr1[] =  "wellcomg to CHINA!!!" ;
+	char arr2[] =  "###################";
+	int left = 0;
+	int right = strlen(arr1) - 2;///sizeof(arr1) / sizeof(arr1[0])-2;
+	while (left<=right)
 	{
-		b = b * i;
-		c = c+ b;
+		arr2[left] = arr1[left];
+		arr2[right] = arr1[right];
+		printf("%s\n", arr2);
+		left++;
+		right--;
+		Sleep(1000);
+
 	}
-	
-	printf("阶乘等于%d",c);
 	return 0;
 }
+
+
+
+//int main()
+//{
+//	int c = 0;
+//	int b = 1;
+//	int i = 0;
+//	for (i = 1; i <=3; i++)
+//	{
+//		b = b * i;
+//		c = c+ b;
+//	}
+//	
+//	printf("阶乘等于%d",c);
+//	return 0;
+//}
 
 //int main() 
 //{
